50ea3b2aa84681ed83b437f8ecfdc15488b8deb8
Godwin Archibong <sparkdicey@gmail.com>
2023-01-23 15:46:39 +0100

    animations added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // alert("godwin code is working")
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("show")
+        } else{
+            entry.target.classList.remove("show")
+        }
+    });
+});
+
+const hiddenElement = document.querySelectorAll("scrollanimation");
+hiddenElement.forEach((el) => observer.observe(el));
